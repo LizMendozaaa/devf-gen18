2c90135028e265520b79c4f0c2a2c7894190f88a
Lizeth Mendoza <kittylizze@gmail.com>
2022-07-11 20:53:47 -0500

    added index and average.js

---
+++ kata-javascript/05-clase-2022-07-11/average.js
@@ -0,0 +1,10 @@
+// Crear archivo HTML y conectarlo con nuestro archivo JS
+// Crear 5 prompts que cada uno reciba una calificacion, un total de 5 calificaciones
+// convertir los prompts a tipo number 
+// Crear una funcion que reciba por parametro las 5 calificaciones y 
+// Calcule el promedio de esas notas.
+// Cuando tengamos el promedio vamos a validar lo siguiente:
+// Si el promedio es igual a 10 vamos a retonar un mensaje para felicitar al alumno
+// si el promedio es mayor que 8 vamos a retornar un mensaje que diga "Casi, casi"
+// Si el promedio es mayor que 6 pero menor que 8, retornamos mensaje que diga "Hay que ponerle mas ganas"
+// Si el resultado es menor que 6 retornamos mensaje que diga "No pasaste el grado"
