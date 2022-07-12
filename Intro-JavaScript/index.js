61ca423a92a56555690399059a6a8e51071fc507
Lizeth Mendoza <kittylizze@gmail.com>
2022-07-11 20:46:04 -0500

    added lots of files

---
+++ Intro-JavaScript/index.js
@@ -0,0 +1,8 @@
+var nombreUsuario = prompt('Ingresa tu nombre')
+var edadUsuario = prompt('Ingresa tu edad');
+var hobby = prompt('Ingresa tu hobbie')
+
+console.log('Bienvenido ', nombreUsuario)
+console.log ('Bienvenido', nombreUsuario, 'que tiene' edadUsuario, 'años, que disfruta', hobbys)
+
+document.write(nombreUsuario)
